a7708d83087aa0fa51777d62a1f44d3a79999c33
David Roda <davidcroda@gmail.com>
2014-02-15 20:23:14 -0500

    More logging

--- app/middleware/token.js
+++ app/middleware/token.js
@@ -1,5 +1,7 @@
 
 exports.authorize = function(req, res, next) {
+  console.log("Auth: " + req.isAuthenticated());
+  console.log("Path: " + req.path);
   if(req.path == '/l' || req.isAuthenticated() || req.headers['x-token'] == process.env.TOKEN) {
     return next();
   } else {
