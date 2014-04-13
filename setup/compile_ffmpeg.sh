72e875509378f01178da5c994a38ac29f879d2e9
David Roda <davidcroda@gmail.com>
2014-04-13 10:25:02 -0400

    Update ffmpeg compile script

--- setup/compile_ffmpeg.sh
+++ setup/compile_ffmpeg.sh
@@ -4,31 +4,32 @@ sudo apt-get update
 sudo apt-get -y install autoconf automake build-essential libass-dev libgpac-dev \
   libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
   libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev, unzip, libmp3lame-dev
-mkdir ~/ffmpeg_sources
+
+HOME="$HOME/src/"
 
 
-cd ~/ffmpeg_sources
+cd $HOME/ffmpeg_sources
 wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
 tar xzvf yasm-1.2.0.tar.gz
 cd yasm-1.2.0
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+./configure --prefix="$HOME/ffmpeg_build" --bindir="~/bin"
 make
 make install
 make distclean
-export "PATH=$PATH:$HOME/bin"
+export "PATH=$PATH:~/bin"
 
 
-cd ~/ffmpeg_sources
+cd $HOME/ffmpeg_sources
 wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
 tar xjvf last_x264.tar.bz2
 cd x264-snapshot*
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+./configure --prefix="$HOME/ffmpeg_build" --bindir="~/bin" --enable-static
 make
 make install
 make distclean
 
 
-cd ~/ffmpeg_sources
+cd $HOME/ffmpeg_sources
 wget -O fdk-aac.zip https://github.com/mstorsjo/fdk-aac/zipball/master
 unzip fdk-aac.zip
 cd mstorsjo-fdk-aac*
@@ -38,7 +39,7 @@ make
 make install
 make distclean
 
-cd ~/ffmpeg_sources
+cd $HOME/ffmpeg_sources
 wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
 tar xzvf opus-1.1.tar.gz
 cd opus-1.1
@@ -47,7 +48,7 @@ make
 make install
 make distclean
 
-cd ~/ffmpeg_sources
+cd $HOME/ffmpeg_sources
 wget http://webm.googlecode.com/files/libvpx-v1.3.0.tar.bz2
 tar xjvf libvpx-v1.3.0.tar.bz2
 cd libvpx-v1.3.0
@@ -56,14 +57,14 @@ make
 make install
 make clean
 
-cd ~/ffmpeg_sources
+cd $HOME/ffmpeg_sources
 wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
 export PKG_CONFIG_PATH
 ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" \
-   --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs=-ldl --enable-gpl \
+   --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="~/bin" --extra-libs=-ldl --enable-gpl \
    --enable-libass --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-libtheora \
    --enable-libvorbis --enable-libvpx --enable-libx264 --enable-nonfree
 make
