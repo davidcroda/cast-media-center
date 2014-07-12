f662f919a0d526ab2e60d808daa1ff969ae7f324
David Roda <davidcroda@gmail.com>
2014-07-12 08:35:25 -0400

    Implement basic login service

--- client/js/app.js
+++ client/js/app.js
@@ -12,7 +12,16 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/video_list.html', controller: 'VideoListController'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginController'});
   $routeProvider.when('/sources', {templateUrl: 'partials/sources.html', controller: 'SourceController'});
   $routeProvider.when('/videos/:id', {templateUrl: 'partials/video.html', controller: 'VideoController'});
   $routeProvider.otherwise({redirectTo: '/'});
-}]);
+}])
+.constant('AUTH_EVENTS', {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+  });
