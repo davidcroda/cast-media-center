6e21fcbabc8d8c41c662055f0f62c7af5e72cf78
David Roda <davidcroda@gmail.com>
2014-07-12 08:40:25 -0400

    fix api base

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -6,7 +6,7 @@ angular.module('myApp.controllers', [])
 
   .controller('VideoListController', ['$scope', '$http', '$location', 'chromecast', function ($scope, $http, $location, chromecast) {
     chromecast = new chromecast($scope);
-    $http.get('http://localhost:3000/api/video').success(function (data) {
+    $http.get('http://daveroda.com/api/video').success(function (data) {
       var videos = data.video;
       angular.forEach(videos, function (video, key) {
         video.canPlay = (video.vcodec == "h264" && video.acodec == "aac");
@@ -36,32 +36,32 @@ angular.module('myApp.controllers', [])
     };
 
     $scope.deleteVideo = function (video) {
-      $http.delete('http://localhost:3000/api/video/' + video.id);
+      $http.delete('http://daveroda.com/api/video/' + video.id);
     };
 
   }])
 
   .controller('VideoController', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
-    $http.get('http://localhost:3000/api/video/' + $routeParams.id).success(function (data) {
+    $http.get('http://daveroda.com/api/video/' + $routeParams.id).success(function (data) {
       $scope.phone = data;
     });
   }])
 
   .controller('SourceController', ['$scope', '$http', function ($scope, $http) {
     $scope.index = function () {
-      $http.get('http://localhost:3000/api/source').success(function (data) {
+      $http.get('http://daveroda.com/api/source').success(function (data) {
         $scope.sources = data.source;
       });
     };
 
     $scope.add = function (source) {
-      $http.put('http://localhost:3000/api/source', source).success(function () {
+      $http.put('http://daveroda.com/api/source', source).success(function () {
         $scope.index();
       });
     };
 
     $scope.delete = function (source) {
-      $http.delete('http://localhost:3000/api/source/' + source._id).success(function () {
+      $http.delete('http://daveroda.com/api/source/' + source._id).success(function () {
         $scope.index();
       })
     };
